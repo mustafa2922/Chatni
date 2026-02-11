b1af9437e7062469f110bcc502e4d0a0f8395cf3
Mustafa <mustafaraza6372@gmail.com>
2026-02-12 01:00:52 +0500

    rate limiting implemented

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,27 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const aj = arcjet({
+
+    key: process.env.ARCJET_KEY,
+    rules: [
+      
+        shield({ mode: "LIVE" }),
+
+        detectBot({
+            mode: "LIVE",
+            allow: [
+                "CATEGORY:SEARCH_ENGINE",
+            ],
+        }),
+
+        slidingWindow({
+            mode: "LIVE",
+            max:100,
+            interval:60
+        }),
+    ],
+});
+
+export default aj;
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import { signup, login, logout, updateProfile } from '../controllers/auth.controllers.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
+import arcjetProtection from '../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
-router.post('/signup',signup);
-router.post('/login',login);
-router.post('/logout',logout);
+router.use(arcjetProtection);
 
-router.put('/update-profile',protectRoute,updateProfile);
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
 
-router.get('/check',protectRoute,(req,res) => res.status(200).json(req.user));
+router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
